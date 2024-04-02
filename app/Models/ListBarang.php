ead1f18e709f0d603a9c9a8eff172d40f53f0f08
Gunawan19621 <gunawan19621@gmail.com>
2024-04-02 14:12:12 +0700

    update marge List Barang

--- app/Models/ListBarang.php
+++ app/Models/ListBarang.php
@@ -3,10 +3,29 @@
 namespace App\Models;
 
 use App\Traits\UUID;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\SuratJalan;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ListBarang extends Model
 {
     use HasFactory;
+    protected $table = 'list_barangs';
+    protected $guarded = [];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
+
+    public function suratJalan()
+    {
+        return $this->belongsTo(SuratJalan::class, 'id_sj');
+    }
+
+    public function pabrik()
+    {
+        return $this->belongsTo(Pabrik::class, 'id_pabrik');
+    }
 }
